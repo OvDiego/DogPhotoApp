931641cd2438ad4313a6c3fb6398c91fe14b5430
Diego <diego.cruzov@ingenieria.unam.edu>
2024-04-09 20:59:17 -0600

    Add Rules

--- app/src/test/java/com/example/dogphotoapp/fake/DogViewModelTest.kt
+++ app/src/test/java/com/example/dogphotoapp/fake/DogViewModelTest.kt
@@ -1,12 +1,16 @@
 package com.example.dogphotoapp.fake
 
+import com.example.dogphotoapp.fake.rules.TestDispatcherRule
 import com.example.dogphotoapp.viewmodel.DogUiState
 import com.example.dogphotoapp.viewmodel.DogViewModel
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 
 class DogViewModelTest {
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
     @Test
     fun dogViewModel_getDogPhotos_verifyDogUiStateSuccess(){
         runTest {
